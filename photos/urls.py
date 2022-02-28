98f6b878bdab902c6665dbecc1d08f64d0ac06a7
josephndegwa <kariukindegwa98@gmail.com>
2022-02-28 12:23:18 +0300

    add urls and views path

---
+++ photos/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('<int:photo_id>/', views.photo, name="photos"),
+    path('search/', views.search_results, name="search_results")
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
