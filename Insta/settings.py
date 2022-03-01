70be3a93211978f3d3fec97deb5ac5c6667d1325
josephndegwa <kariukindegwa98@gmail.com>
2022-03-01 23:42:47 +0300

    add static file

--- Insta/settings.py
+++ Insta/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-slyznkn8()deszatbn@&59sd**vapk22gu5c^i+ttiftatb0s8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-slyznkn8()deszatbn@&59sd**vapk22gu5c^i+ttiftatb0s8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,8 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
